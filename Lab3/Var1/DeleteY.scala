32ea4f329dffd17fd5fa220ba9bb6793e4457a46
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-10-23 16:31:24 +0300

    Update DeleteY.scala
    Added another method (for the sake of another method)

--- Lab3/Var1/DeleteY.scala
+++ Lab3/Var1/DeleteY.scala
@@ -2,7 +2,7 @@ package Var1
 
 import scala.util.matching.Regex
 
-object Main17 {
+object Main17_a {
   def main(args: Array[String]): Unit = {
     val input = "Hello to everybody"
     println(input)
@@ -10,6 +10,31 @@ object Main17 {
     val pattern: Regex = "y|Y".r
 
     val result = pattern.replaceAllIn(input, "")
+
+    println("Without y's: " + result)
+  }
+}
+
+object Main17_b {
+  def main(args: Array[String]): Unit = {
+    val input = "Hello to everybody"
+    println(input)
+
+    val pattern: Regex = "y|Y".r
+
+    val matched = pattern.findAllMatchIn(input)
+
+    var offset = 0
+
+    val result = matched.foldLeft(input)((str, matched_data) => {
+      val start = matched_data.start
+      val end = matched_data.end
+      val temp = offset
+
+      offset += end - start
+
+      str.substring(0, start-temp) + str.substring(end-temp)
+    })
 
     println("Without y's: " + result)
   }
