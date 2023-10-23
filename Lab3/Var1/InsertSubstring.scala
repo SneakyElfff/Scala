55a871c257af2f98a70392f67307203ae5db1f76
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-10-23 16:32:12 +0300

    Update InsertSubstring.scala
    Added another variant of realisation (redundant)

--- Lab3/Var1/InsertSubstring.scala
+++ Lab3/Var1/InsertSubstring.scala
@@ -2,13 +2,33 @@ package Var1
 
 import scala.util.matching.Regex
 
-object Main18 {
+object Main18_a {
   def main(args: Array[String]): Unit = {
     val input = "Hello to everybody"
 
     val pattern: Regex = "Hello".r
 
     val result = pattern.replaceFirstIn(input, "$0 with heartiness")
+
+    println(result)
+  }
+}
+
+object Main18_b {
+  def main(args: Array[String]): Unit = {
+    val input = "Hello to everybody"
+    val substr = "with heartiness"
+
+    val pattern: Regex = "Hello".r
+
+    val matched = pattern.findAllMatchIn(input)
+
+    val result = matched.foldLeft(input)((str, matched_data) => {
+      val start = matched_data.start
+      val end = matched_data.end
+
+      str.substring(0, end+1) + substr + str.substring(end)
+    })
 
     println(result)
   }
